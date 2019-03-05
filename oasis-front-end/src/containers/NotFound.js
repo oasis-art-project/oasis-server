b5b6d3997f91a8c1cf6c3198cfde8b4b83fffb9e
icnt <772611369sola@gamil.ocm>
2019-03-04 22:55:55 -0500

    Add initial code

---
+++ oasis-front-end/src/containers/NotFound.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./NotFound.css";
+
+export default () =>
+    <div className="NotFound">
+        <h1>404!</h1>
+        <h3>Sorry, this page is traveling outside</h3>
+    </div>;
