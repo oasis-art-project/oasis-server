7870651968851c4e8e9f1b485e52b65f07bbf4ad
Maxim Tsybanov <github@tsybanov.com>
2019-03-21 00:16:32 -0400

    Fixed initial script from 'home' to 'views' in templates/__init__.py

--- templates/__init__.py
+++ templates/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 app = Flask(__name__,
  static_folder = './public',
  template_folder="./static")
-from templates.oasis.home import home_blueprint
+from templates.oasis.views import home_blueprint
 
 # register the blueprints
 app.register_blueprint(home_blueprint)
