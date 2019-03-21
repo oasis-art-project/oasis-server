c7130e094129c7eefdca3599793f11ed6bab86a6
Maxim Tsybanov <github@tsybanov.com>
2019-03-21 16:33:08 -0400

    Fixed paths in the webpack.config.js due to the moving of file itself

--- src/webpack.config.js
+++ src/webpack.config.js
@@ -3,11 +3,11 @@ const resolve = require('path').resolve;
 
 const config = {
         devtool: 'eval-source-map',
- 	entry: __dirname + '/js/index.jsx',
+ 	entry: __dirname + '/static/js/index.jsx',
  	output:{
-      path: resolve('/src/public/'),
+      path: resolve('public/'),
       filename: 'bundle.js',
-      publicPath: resolve('/src/public/')
+      publicPath: resolve('public/')
  	},
  	resolve: {
   	extensions: ['.js','.jsx','.css']
