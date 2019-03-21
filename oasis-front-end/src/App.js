2a5c8e58531aabe1a993ea729c708c60b1946685
Gahouray <gdukuray@gmail.com>
2019-03-21 01:14:30 -0400

    Tried Adding Event Page, Link is having trouble. Got some cards to display events. Also added Footer

--- oasis-front-end/src/App.js
+++ oasis-front-end/src/App.js
@@ -5,7 +5,37 @@ import "./App.css";
 import Routes from "./Routes";
 import { LinkContainer } from "react-router-bootstrap";
 import {Button} from "react-bootstrap";
-import EventPage from "./containers/EventPage";
+import {Col, Row} from "reactstrap";
+
+const footerStyle = {
+  backgroundColor: "#C5DDDB",
+  fontSize: "20px",
+  color: "white",
+  borderTop: "1px solid #E7E7E7",
+  textAlign: "center",
+  padding: "20px",
+  position: "fixed",
+  left: "0",
+  bottom: "0",
+  height: "60px",
+  width: "100%"
+};
+
+const phantomStyle = {
+  display: "block",
+  padding: "20px",
+  height: "60px",
+  width: "100%"
+};
+
+function Footer({ children }) {
+  return (
+      <div>
+        <div style={phantomStyle} />
+        <div style={footerStyle}>{children}</div>
+      </div>
+  );
+}
 
 
 class App extends Component {
@@ -34,8 +64,12 @@ class App extends Component {
             </Navbar.Collapse>
           </Navbar>
           <Routes />
+          <Footer>
+            <p>&copy; OASIS 2019</p>
+          </Footer>
 
         </div>
+
 
 
     );
