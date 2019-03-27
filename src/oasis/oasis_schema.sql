e727aaf1e7091c94129eaee88a19ce4066c2fb60
cwille97 <cwille97@gmail.com>
2019-03-26 21:17:24 -0400

    added events

--- src/oasis/oasis_schema.sql
+++ src/oasis/oasis_schema.sql
@@ -134,7 +134,25 @@ INSERT INTO places (`id`, `place_name`, `owner_id`, `loc_lon`, `loc_lat`) VALUES
 INSERT INTO places (`id`, `place_name`, `owner_id`, `loc_lon`, `loc_lat`) VALUES ('104', 'Garden', '2', '42.360087', '-71.065992');
 INSERT INTO places (`id`, `place_name`, `owner_id`, `loc_lon`, `loc_lat`) VALUES ('105', 'Wall on Mass', '5', '42.367756', '-71.107348');
 
-/* Example DB entries for events */
+/* Example DB entries for events YYYY-MM-DD HH:MI:SS */
+
+INSERT INTO events (`id`, `place_id`, `artist_id`, `description`, `start_date`, `end_date`, `photo`) VALUES ('500', '100', '8', '', '2019-03-27 00:00:00', '2019-04-27 00:00:00', '');
+INSERT INTO events (`id`, `place_id`, `artist_id`, `description`, `start_date`, `end_date`, `photo`) VALUES ('501', '101', '9', '', '2019-02-01 00:00:00', '', ''); 
+INSERT INTO events (`id`, `place_id`, `artist_id`, `description`, `start_date`, `end_date`, `photo`) VALUES ('502', '102', '10', '', '2019-04-01 00:00:00', '2019-04-05 00:00:00', ''); 
+INSERT INTO events (`id`, `place_id`, `artist_id`, `description`, `start_date`, `end_date`, `photo`) VALUES ('503', '103', '11', '', '2019-03-01 00:00:00', '', ''); 
+INSERT INTO events (`id`, `place_id`, `artist_id`, `description`, `start_date`, `end_date`, `photo`) VALUES ('504', '104', '11', '', '2019-03-01 00:00:00', '2019-03-10 00:00:00', ''); 
+INSERT INTO events (`id`, `place_id`, `artist_id`, `description`, `start_date`, `end_date`, `photo`) VALUES ('505', '105', '12', '', '2019-03-01 00:00:00', '2019-04-01 18:00:00', ''); 
+INSERT INTO events (`id`, `place_id`, `artist_id`, `description`, `start_date`, `end_date`, `photo`) VALUES ('506', '100', '13', '', '2019-03-01 00:00:00', '', ''); 
+INSERT INTO events (`id`, `place_id`, `artist_id`, `description`, `start_date`, `end_date`, `photo`) VALUES ('507', '100', '14', '', '2019-03-01 00:00:00', '', ''); 
+INSERT INTO events (`id`, `place_id`, `artist_id`, `description`, `start_date`, `end_date`, `photo`) VALUES ('508', '101', '15', '', '2019-03-01 00:00:00', '', ''); 
+INSERT INTO events (`id`, `place_id`, `artist_id`, `description`, `start_date`, `end_date`, `photo`) VALUES ('509', '101', '16', '', '2019-03-01 00:00:00', '', ''); 
+INSERT INTO events (`id`, `place_id`, `artist_id`, `description`, `start_date`, `end_date`, `photo`) VALUES ('510', '102', '17', '', '2019-03-01 00:00:00', '', ''); 
+INSERT INTO events (`id`, `place_id`, `artist_id`, `description`, `start_date`, `end_date`, `photo`) VALUES ('511', '102', '18', '', '2019-03-01 00:00:00', '', ''); 
+INSERT INTO events (`id`, `place_id`, `artist_id`, `description`, `start_date`, `end_date`, `photo`) VALUES ('512', '103', '19', '', '2019-03-01 00:00:00', '', ''); 
+INSERT INTO events (`id`, `place_id`, `artist_id`, `description`, `start_date`, `end_date`, `photo`) VALUES ('513', '103', '20', '', '2019-03-01 00:00:00', '', ''); 
+INSERT INTO events (`id`, `place_id`, `artist_id`, `description`, `start_date`, `end_date`, `photo`) VALUES ('514', '104', '21', '', '2019-03-01 00:00:00', '', ''); 
+INSERT INTO events (`id`, `place_id`, `artist_id`, `description`, `start_date`, `end_date`, `photo`) VALUES ('515', '104', '22', '', '2019-03-01 00:00:00', '', ''); 
+INSERT INTO events (`id`, `place_id`, `artist_id`, `description`, `start_date`, `end_date`, `photo`) VALUES ('516', '105', '23', '', '2019-04-01 08:00:00', '', ''); 
 
 -- End of file.
 
