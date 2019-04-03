2f4b160844bcf479fe57ab5071f03488605f03bf
Maxim Tsybanov <github@tsybanov.com>
2019-04-03 00:16:44 -0400

    Create initial template of auth.py

---
+++ src/oasis/auth.py
@@ -0,0 +1,25 @@
+import os
+from flask import request, jsonify
+from flask_jwt_extended import (create_access_token, create_refresh_token,
+                                jwt_required, jwt_refresh_token_required, get_jwt_identity)
+from app import app, flask_bcrypt, jwt
+from jsonschema import validate_user
+
+@jwt.unauthorized_loader
+def unauthorized_response(callback):
+    return jsonify({
+        'ok': False,
+        'message': 'Missing Authorization Header'
+    }), 401
+
+
+@app.route('/auth', methods=['POST'])
+def auth_user():
+    data = validate_user(request.get_json())
+    if data['ok']:
+            print(data['data'])
+            return jsonify({'ok': True, 'data': user}), 200
+        else:
+            return jsonify({'ok': False, 'message': 'invalid username or password'}), 401
+    else:
+        return jsonify({'ok': False, 'message': 'Bad request parameters: {}'.format(data['message']
