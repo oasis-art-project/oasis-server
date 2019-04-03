c6d96b47f89c3889401458d3bce1a5e92662e589
Maxim Tsybanov <github@tsybanov.com>
2019-04-03 00:17:06 -0400

    Addition to the previous commit

--- src/oasis/views.py
+++ src/oasis/views.py
@@ -1,5 +1,10 @@
 from flask import render_template, Blueprint
+from auth import auth
 home_blueprint = Blueprint('home',__name__)
 @home_blueprint.route('/')
 def index():
  return render_template("index.html")
+
+@home_blueprint.route('/api/auth', methods=['POST'])
+def auth_user():
+ return auth()
