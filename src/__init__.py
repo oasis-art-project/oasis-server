aceb58be3c6850769afa4340869b9ffaafa78fcf
Maxim Tsybanov <github@tsybanov.com>
2019-04-02 23:52:38 -0400

    Added JSONEncoder class to __init__ backend

--- src/__init__.py
+++ src/__init__.py
@@ -9,6 +9,18 @@ app = Flask(__name__,
  template_folder="./static")
 from src.oasis.views import home_blueprint
 
+class JSONEncoder(json.JSONEncoder):
+    ''' extend json-encoder class'''
+
+    def default(self, o):
+        if isinstance(o, ObjectId):
+            return str(o)
+        if isinstance(o, set):
+            return list(o)
+        if isinstance(o, datetime.datetime):
+            return str(o)
+        return json.JSONEncoder.default(self, o)
+
 # register the blueprints
 app.register_blueprint(home_blueprint)
 
