93987d2f7468ae8f5895336ccd13f0a7925f3e43
Maxim Tsybanov <github@tsybanov.com>
2019-04-03 16:27:22 -0400

    Added initial query template from database thru find_user method from db in src/oasis/auth.py

--- src/oasis/auth.py
+++ src/oasis/auth.py
@@ -2,8 +2,9 @@ import os
 from flask import request, jsonify
 from flask_jwt_extended import (create_access_token, create_refresh_token,
                                 jwt_required, jwt_refresh_token_required, get_jwt_identity)
-from src import app
+#from src import flask_bcrypt, jwt
 from .schemas import validate_user
+from .users import find_user
 
 #@jwt.unauthorized_loader
 #def unauthorized_response(callback):
@@ -16,7 +17,10 @@ from .schemas import validate_user
 def auth(req):
     data = validate_user(req)
     if data['ok']:
-            return data['data']['email']
+            data = data['data']
+            user = find_user(data['email'])
+            print(user)
+            return data['email']
             #return jsonify({'ok': True, 'data': user}), 200
     else:
         return jsonify({'ok': False, 'message': 'Bad request parameters: {}'.format(data['message'])}), 400
