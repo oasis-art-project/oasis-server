ca9d578891cd0c9e4874fa7b0a01d946d9bd671a
Maxim Tsybanov <github@tsybanov.com>
2019-04-03 15:12:11 -0400

    Moved registration of Bluprints from src/ to src/oasis/views.py to get rid of circulation import

--- src/__init__.py
+++ src/__init__.py
@@ -7,11 +7,8 @@ from flask_bcrypt import Bcrypt
 app = Flask(__name__,
  static_folder = './public',
  template_folder="./static")
-from src.oasis.views import home_blueprint
 
 class JSONEncoder(json.JSONEncoder):
-    ''' extend json-encoder class'''
-
     def default(self, o):
         if isinstance(o, ObjectId):
             return str(o)
@@ -20,9 +17,6 @@ class JSONEncoder(json.JSONEncoder):
         if isinstance(o, datetime.datetime):
             return str(o)
         return json.JSONEncoder.default(self, o)
-
-# register the blueprints
-app.register_blueprint(home_blueprint)
 
 app.config.from_mapping(
     SECRET_KEY='dev',
@@ -33,7 +27,6 @@ app.config['JWT_ACCESS_TOKEN_EXPIRES'] = datetime.timedelta(days=1)
 flask_bcrypt = Bcrypt(app)
 jwt = JWTManager(app)
 app.json_encoder = JSONEncoder
-
 # ensure the instance folder exists
 try:
     os.makedirs(app.instance_path)
--- src/oasis/views.py
+++ src/oasis/views.py
@@ -1,6 +1,10 @@
 from flask import render_template, Blueprint, request
+from src import app
 from .auth import auth
 home_blueprint = Blueprint('home',__name__)
+
+app.register_blueprint(home_blueprint)
+
 @home_blueprint.route('/')
 def index():
  return render_template("index.html")
