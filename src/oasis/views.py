123f7710451e6b7941322b99b4ef562a50824232
Maxim Tsybanov <github@tsybanov.com>
2019-04-04 00:19:22 -0400

    Deleted routes for auth since auth_blueprint was created for this purpose

--- src/oasis/views.py
+++ src/oasis/views.py
@@ -1,16 +1,7 @@
 from flask import render_template, Blueprint, request
-from .auth import auth
 
 home_blueprint = Blueprint('home', __name__)
 
 @home_blueprint.route('/')
 def index():
  return render_template("index.html")
-
-@home_blueprint.route('/api/auth', methods=['POST'])
-def auth_user():
- return auth(request.get_json(force=True))
-
-@home_blueprint.route('/api/register', methods=['POST'])
-def register():
- return register(request.get_json(force=True))
