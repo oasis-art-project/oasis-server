3e3b1d8f0a6230942eafd349a544b24fe47e9f00
Maxim Tsybanov <github@tsybanov.com>
2019-04-04 00:17:49 -0400

    Moved import and registration of blueprints in src/__init__.py to the end of file for avoiding circular imports

--- src/__init__.py
+++ src/__init__.py
@@ -7,7 +7,6 @@ from flask_bcrypt import Bcrypt
 app = Flask(__name__,
  static_folder = './public',
  template_folder="./static")
-from src.oasis.views import home_blueprint
 
 class JSONEncoder(json.JSONEncoder):
     def default(self, o):
@@ -23,7 +22,6 @@ app.config.from_mapping(
     SECRET_KEY='dev',
     DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
 )
-app.register_blueprint(home_blueprint)
 app.config['JWT_SECRET_KEY'] = os.environ.get('SECRET')
 app.config['JWT_ACCESS_TOKEN_EXPIRES'] = datetime.timedelta(days=1)
 flask_bcrypt = Bcrypt(app)
@@ -34,6 +32,11 @@ try:
     os.makedirs(app.instance_path)
 except OSError:
     pass
+
+from src.oasis.views import home_blueprint
+from src.oasis.auth import auth_blueprint
+app.register_blueprint(auth_blueprint)
+app.register_blueprint(home_blueprint)
 
 from src.oasis import db
 db.init_app(app)
