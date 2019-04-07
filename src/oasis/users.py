76c4fde3fed3f5273e0f051316119dc4eb34f172
Maxim Tsybanov <github@tsybanov.com>
2019-04-06 23:43:10 -0400

    Modified create_user method in the users model users.py

--- src/oasis/users.py
+++ src/oasis/users.py
@@ -1,62 +1,19 @@
 from flask import request
 from .db import get_db
 
-def create_user(data):
-        user = get_db().execute(
-                'SELECT id FROM users WHERE email = ?',
-                (data['email'],)
-        ).fetchone()
-
-        # Return None if a user exists
-        if user is not None:
-            return None 
-
+def create_user(user):
+        get_db().execute('INSERT INTO users (first_name, last_name, email, user_password, user_role) Values (?, ?, ?, ?, ?)',
+			(user["firstName"], user["lastName"], user["email"], user["password"], user["role"]))
+	db.commit() 
+        
         return user
 
-        
-        """
-        if request.method == 'POST':
-		first_name = request.form['first_name']
-		last_name = request.form['last_name']
-		email = request.form['email']
-		user_password = request.form['user_password']
-		user_role = request.form['user_role'] # an integer of 1 represents Admin, 2 is 								Host, 3 is Artist, 4 is Visitor
-		# user_id = some logic to generate a user ID
-		# some logic to check if the email is valid i.e. long enough, valid characters
-		# some logic to check if the password is valid i.e. long enough, valid characters
-		# some logic to check if the password matches when it is enterd a second time
-		error = None
-		
-		if not first_name:
-			error = 'A first name must be provided'
-		elif not last_name:
-			error = 'A last name must be provided'
-		elif not email:
-			error = 'A valid email must be provided'
-		elif not user_password:
-			error = 'A valid password must be provided' 
-		elif not user_role:
-			error = 'A valid user role must be provided'
-		else:
-			db = get_db()
-			db.execute(
-				'INSERT INTO users (id, first_name, last_name, email, 					user_password, user_role)'
-				' Values (?, ?, ?, ?, ?, ?)',
-				(user_id, first_name, last_name, email, user_password, user_role)
-			)
-			db.commit()
-			# return redirect(url_for()) some url to take the user to the homepage
-	# return render_template()
-        """
 def get_user(user_id):
 	user = get_db().execute(
 		'SELECT id, first_name, last_name, email, user_password, user_role FROM users WHERE id = ?',
 		(user_id,)
 	).fetchone()
 	
-	if user is None:
-            return None
-            # abort(404, "User ID {0} doesn't exist.".format(id)) code to handle failure
 	return user
 
 def find_user(email):
@@ -64,9 +21,6 @@ def find_user(email):
                 'SELECT id, first_name, last_name, email, user_password, user_role FROM users WHERE email = ?',
                 (email,)
         ).fetchone()
-
-        if user is None:
-            return None
 
         return user
 
