340b708dde718a57c31d009f1a55b47b4729cc7d
icnt <772611369sola@gamil.ocm>
2019-04-09 23:59:23 -0400

    signup page

--- src/static/js/Routes.js
+++ src/static/js/Routes.js
@@ -3,6 +3,7 @@ import { Route, Switch } from "react-router-dom";
 import Home from "./containers/Home";
 import NotFound from "./containers/NotFound"
 import Login from "./containers/Login";
+import Signup from "./containers/Signup";
 
 export default () =>
     <Switch>
