6d6280eaeca6fc36df9e518dd0f2062ce8d0f1fc
codeanticode <andres.colubri@gmail.com>
2019-05-02 14:38:47 -0400

    starting writing a simple script to test the API

---
+++ scripts/populate.py
@@ -0,0 +1,12 @@
+import requests
+
+resp = requests.get('http://127.0.0.1:5000/api/event')
+
+if resp.status_code != 200:
+    # This means something went wrong.
+    raise Exception(resp.status_code)
+
+print("Request was succesful!")
+print("Got the following:")
+for item in resp.json():
+    print(item)
