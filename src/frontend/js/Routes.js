344ea8f436d2f0e8c89bf611450e411638da6212
Cedric <cjwille@gmail.com>
2019-05-01 21:55:54 -0400

    moved artist details and place details over and added routing

--- src/frontend/js/Routes.js
+++ src/frontend/js/Routes.js
@@ -3,11 +3,15 @@ import { Route, Switch } from "react-router-dom";
 import Home from "./containers/Home";
 import NotFound from "./containers/NotFound"
 import Login from "./containers/Login";
+import ArtistDetails from "./containers/ArtistDetails"
+import PlaceDetails from "./containers/PlaceDetails"
 
 export default () =>
     <Switch>
         <Route path="/" exact component={Home} />
         <Route path="/login" exact component={Login} />
+        <Route path="/ArtistDetails" component={ArtistDetails} />
+        <Route path="/PlaceDetails" component={PlaceDetails} />
         {/*Catch all unmatched routes and redirected to 404*/}
         <Route component={NotFound} />
 
