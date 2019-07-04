26b25e1b0f2a163df70b949f346787e161a239d5
codeanticode <andres.colubri@gmail.com>
2019-07-04 19:48:25 +0900

    fixed url

--- scripts/populate.py
+++ scripts/populate.py
@@ -1,20 +1,20 @@
 import requests
 
-user_data = {'email': "foobarcom",
-             'password': "foo",
-             'firstName': "",
-             'lastName': " ",
-             'twitter': "http://",
-             'flickr': "http://",
-             'instagram': "http://",
-             'role': 6
+user_data = {"email": "vincent@oasis.art",
+             "password": "art",
+             "firstName": "Vincent",
+             "lastName": "Van Gogh",
+             "twitter": "laOrejaDeVanGogh",
+             "flickr": "laNarizDeVanGogh",
+             "instagram": "elOjoDeVanGogh",
+             "role": 2
             }
 
-r = requests.post('http://127.0.0.1:5000/api/user', data = user_data)
+r = requests.post('http://127.0.0.1:5000/api/user/', data = user_data)
 
 print(r.status_code)
 
-if r.status_code != 200:
+if r.status_code != 201:
     # This means something went wrong.
     raise Exception(r.status_code)
 
@@ -22,15 +22,3 @@ print("Request was succesful!")
 print("Got the following:")
 for item in r.json():
     print(item)
-
-
-# r = requests.get('http://127.0.0.1:5000/api/user')
-
-# if r.status_code != 200:
-#     # This means something went wrong.
-#     raise Exception(r.status_code)
-
-# print("Request was succesful!")
-# print("Got the following:")
-# for item in r.json():
-#     print(item)
