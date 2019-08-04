6248e4846ba208e00818b7788428eeec879afeea
codeanticode <andres.colubri@gmail.com>
2019-08-04 17:01:45 -0400

    removed files used for deployment in now.sh

--- index.py
+++
@@ -1,9 +0,0 @@
-from src.app import create_app
-
-app = create_app()
-app.run()
-
-# @app.route('/', defaults={'path': ''})
-# @app.route('/<path:path>')
-# def catch_all(path):
-#     return Response("<h1>Flask on Now</h1><p>You visited: /%s</p>" % (path), mimetype="text/html")
