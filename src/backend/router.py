ec5c06078a77fb6c4e90b299c3b11a69de16b3f0
codeanticode <andres.colubri@gmail.com>
2020-03-03 21:03:58 -0500

    some editing

--- src/backend/router.py
+++ src/backend/router.py
@@ -6,23 +6,23 @@ Copyright (c) 2019 DUOpoly
 License Artistic-2.0
 """
 
-from src.backend.controllers.artworkController import ArtworkResource
-from src.backend.controllers.eventController import EventResource
 from src.backend.controllers.loginController import LoginResource
-from src.backend.controllers.placeController import PlaceResource
 from src.backend.controllers.userController import UserResource
-from src.backend.controllers.s3Controller import S3Resource
+from src.backend.controllers.artworkController import ArtworkResource
+from src.backend.controllers.placeController import PlaceResource
+from src.backend.controllers.eventController import EventResource
 from src.backend.controllers.mediaController import MediaResource
+from src.backend.controllers.s3Controller import S3Resource
 
 def init_router(api):
     """
     Register all router resources
     :param api: api instance passed from extensions.py
     """
+    api.add_resource(LoginResource, '/login/') 
     api.add_resource(UserResource, '/user/')
     api.add_resource(UserResource, '/user/<int:user_id>', endpoint='user_id')
     api.add_resource(UserResource, '/user/<string:user_email>', endpoint='user_email')
-    api.add_resource(LoginResource, '/login/')
     api.add_resource(ArtworkResource, '/artwork/')
     api.add_resource(ArtworkResource, '/artwork/<int:artwork_id>', endpoint='artwork_id')
     api.add_resource(ArtworkResource, '/artwork/artist/<int:artist_id>', endpoint='artist_id')
@@ -32,7 +32,8 @@ def init_router(api):
     api.add_resource(EventResource, '/event/')
     api.add_resource(EventResource, '/event/<int:event_id>', endpoint='event_id')
     api.add_resource(EventResource, '/event/place/<int:place_event_id>', endpoint='place_event_id')
-    api.add_resource(S3Resource, '/s3/')
-    api.add_resource(S3Resource, '/s3/sign/<int:resource_id>', endpoint='s3_sign_id')
     api.add_resource(MediaResource, '/media/')
     api.add_resource(MediaResource, '/media/<int:resource_id>', endpoint='media_id')
+    api.add_resource(S3Resource, '/s3/')
+    api.add_resource(S3Resource, '/s3/sign/<int:resource_id>', endpoint='s3_sign_id')
+    
