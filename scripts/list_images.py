9a49bbffba7f2d0324e55d4f4ff7e2014f55021d
codeanticode <andres.colubri@gmail.com>
2020-03-03 19:15:15 -0500

    added command line args to list_images scripts

--- scripts/list_images.py
+++ scripts/list_images.py
@@ -1,17 +1,15 @@
 import requests
 import json
 import os
+import argparse
 
-use_local_server = False
+parser = argparse.ArgumentParser(description='Upload dummy data to OASIS server.')
+parser.add_argument('-u', '--url', action='store', default='http://127.0.0.1:5000', help='set server url')
+args = parser.parse_args()
 
-if use_local_server:
-    # Local server
-    url = 'http://127.0.0.1:5000'
-else:
-    # Staging server
-    url = 'https://server-oasis.herokuapp.com/'
+server_url = args.url
 
-r = requests.get(url + '/api/user/')
+r = requests.get(server_url + '/api/user/')
 if r.status_code != 200:
     raise Exception(r.status_code)
 users = r.json()['users']
@@ -20,35 +18,35 @@ for user in users:
     role = user['role']
     if role == 1: continue
     print("Images for user", user['firstName'], user["lastName"])
-    r = requests.get(url + '/api/media/' + str(uid) +'?resource-kind=user')
+    r = requests.get(server_url + '/api/media/' + str(uid) +'?resource-kind=user')
     if r.status_code != 200:
         raise Exception(r.status_code)
     j = r.json() 
     for img in j["images"]: 
         print("  ", img)
 
-r = requests.get(url + '/api/place/')
+r = requests.get(server_url + '/api/place/')
 if r.status_code != 200:
     raise Exception(r.status_code)
 places = r.json()['places']
 for place in places:
     pid = place['id']
     print("Images for place", place['name'])
-    r = requests.get(url + '/api/media/' + str(pid) +'?resource-kind=place')
+    r = requests.get(server_url + '/api/media/' + str(pid) +'?resource-kind=place')
     if r.status_code != 200:
         raise Exception(r.status_code)
     j = r.json()
     for img in j["images"]: 
         print("  ", img)
 
-r = requests.get(url + '/api/event/')
+r = requests.get(server_url + '/api/event/')
 if r.status_code != 200:
     raise Exception(r.status_code)
 events = r.json()['events']
 for event in events:
     eid = event['id']
     print("Images for event", event['name'])
-    r = requests.get(url + '/api/media/' + str(eid) +'?resource-kind=event')
+    r = requests.get(server_url + '/api/media/' + str(eid) +'?resource-kind=event')
     if r.status_code != 200:
         raise Exception(r.status_code)
     j = r.json() 
