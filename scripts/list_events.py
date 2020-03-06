0f62afb3c8471d22ee0368213fd556ad82184c39
codeanticode <andres.colubri@gmail.com>
2020-03-05 19:00:36 -0500

    list event script allows to specify date

--- scripts/list_events.py
+++ scripts/list_events.py
@@ -1,13 +1,30 @@
 import requests
 import argparse
 
-parser = argparse.ArgumentParser(description='List events.')
+def print_events(events):
+    for event in events:
+        id = event['id']
+        name = event['name']
+        place = event['place']['name']
+        host = event['place']['host']
+        time0 = event['startTime']
+        time1 = event['endTime']
+        print("===> Event", id, name)
+        print("  Place:", place)
+        print("  Host:", host['firstName'], host['lastName'])
+        print("  Starts:", time0)
+        print("  Ends:", time1)
+
+
+parser = argparse.ArgumentParser(description='List all events, or current and upcoming given a specific date.')
 parser.add_argument('-u', '--url', action='store', default='http://127.0.0.1:5000', help='set server url')
+parser.add_argument('-d', '--date', action='store', default='', help='date to classify events as current and upcoming, in yyy-mm-dd format')
 args = parser.parse_args()
 
 server_url = args.url
+current_date = args.date
 
-r = requests.get(server_url + '/api/event/')
+r = requests.get(server_url + '/api/event/' + current_date)
 
 if r.status_code != 200:
     # This means something went wrong.
@@ -15,16 +32,14 @@ if r.status_code != 200:
 
 print("Request was succesful!")
 print("Got the following:")
-events = r.json()['events']
-for event in events:
-    id = event['id']
-    name = event['name']
-    place = event['place']['name']
-    host = event['place']['host']
-    time0 = event['startTime']
-    time1 = event['endTime']
-    print("===> Event", id, name)
-    print("  Place:", place)
-    print("  Host:", host['firstName'], host['lastName'])
-    print("  Starts:", time0)
-    print("  Ends:", time1)
+if current_date:
+    print("CURRENT EVENTS")
+    current_events = r.json()['current_events']
+    print_events(current_events)
+    print("UPCOMING EVENTS")
+    upcoming_events = r.json()['upcoming_events']
+    print_events(upcoming_events)
+else:
+    print("ALL EVENTS")
+    all_events = r.json()['events']
+    print_events(all_events)
